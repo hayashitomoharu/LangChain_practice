35dd0b0d7a53e7ca10a8e536237a66885375dbb6
hayashitomoharu <146327155+hayashitomoharu@users.noreply.github.com>
2023-11-08 16:25:35 +0900

    fix:by linter

--- src/pinecone_client/vector_db.py
+++ src/pinecone_client/vector_db.py
@@ -3,64 +3,52 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 import os
 from langchain.vectorstores import Pinecone
-from uuid import uuid4
 
-# TODO:以下の関数をクラスにまとめて、重複している共通データをインスタンス変数とかにする
-
-#　環境変数の読み込み
+# 　環境変数の読み込み
 load_dotenv()
 INDEX_NAME = os.getenv("INDEX_NAME")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENV = os.getenv("PINECONE_ENV")
 
+if not INDEX_NAME:
+    raise ValueError("INDEX_NAME 環境変数が設定されていません。")
+if not PINECONE_API_KEY:
+    raise ValueError("PINECONE_API_KEY 環境変数が設定されていません。")
+if not PINECONE_ENV:
+    raise ValueError("PINECONE_ENV 環境変数が設定されていません。")
 
+# pinecone clientの初期化
 pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
 
+# インデックス初期化
+index = pinecone.Index(INDEX_NAME)
 
 # embeddingsの初期化
-model_name = 'text-embedding-ada-002'#TODO env化
+model_name = "text-embedding-ada-002"  # TODO env化
 embeddings = OpenAIEmbeddings(model=model_name)
+
+# TODO:以下の関数をクラスにまとめて、重複している共通データをインスタンス変数とかにする
 
 
 def create_index_or_pass():
+    # pyrightでエラー出るのでつけてます。#TODO:refactor
+    if not INDEX_NAME:
+        raise ValueError("INDEX_NAME 環境変数が設定されていません。")
+
     # すでに作成しているインデックスネームのリストを取得して存在確認
     index_name_list = pinecone.list_indexes()
 
     if INDEX_NAME not in index_name_list:
         pinecone.create_index(
-            INDEX_NAME,
-            dimension=1536,
-            metric="cosine",
-            pod_type="s1"
-            )
-
-        print(pinecone.describe_index(INDEX_NAME))
+            INDEX_NAME, dimension=1536, metric="cosine", pod_type="s1"
+        )
 
     return
 
 
-
 def upsert_vector_from_documents(documents):
-
-    # インデックス初期化
-    index = pinecone.Index(INDEX_NAME)
-    print(index.describe_index_stats())
-
     # TODO:namespaceをユーザーから受け取るかenv変数化
-    vectorstore = Pinecone(index, embeddings, 'testname')
+    vectorstore = Pinecone(index, embeddings, "testname")
 
-    #　documentsを追加
+    # 　documentsを追加
     vectorstore.add_documents(documents)
-
-
-def pinecone_retriever():
-    index = pinecone.Index(INDEX_NAME)
-    # TODO:namespaceをユーザーから受け取るかenv変数化　
-    vectorstore = Pinecone(index, embeddings.embed_query,'testname')
-    retriever = vectorstore.as_retriever(search_type="mmr",serch_kwargs={"k":1})
-    return retriever
-
-
-
-
-
--- src/rag_over_code.py
+++ src/rag_over_code.py
@@ -9,43 +9,58 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationSummaryMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from pinecone_client.vector_db import *
+from pinecone_client.vector_db import (index, create_index_or_pass, upsert_vector_from_documents, embeddings)
 from langchain.vectorstores import Pinecone
+
 # git_clone or 存在確認
 load_dotenv()
 
+
 # ダウンロード先repository
-REPO_URL=os.getenv("REPO_URL")
-BRANCH=os.getenv("BRANCH")
+REPO_URL = os.getenv("REPO_URL")
+BRANCH = os.getenv("BRANCH")
 
 # githubからダウンロードされるものが配置されるディレクトリ
 LOCAL_STRAGE_PATH = os.getenv("LOCAL_STRAGE_PATH")
+
+# 解析するファイルのパス
+ANALIZE_TARGET_PATH = os.getenv("ANALIZE_TARGET_PATH")
+
+# pyright　通らないためNoneにならないことを確定させる
+if not REPO_URL:
+    raise ValueError("REPO_URL 環境変数が設定されていません。")
+if not BRANCH:
+    raise ValueError("BRANCH 環境変数が設定されていません。")
+if not LOCAL_STRAGE_PATH:
+    raise ValueError("LOCAL_STRAGE_PATH 環境変数が設定されていません。")
+if not ANALIZE_TARGET_PATH:
+    raise ValueError("ANALIZE_TARGET_PATH 環境変数が設定されていません。")
+
 
 if os.path.exists(LOCAL_STRAGE_PATH):
     pass
 else:
     print("\ndownloading...\n")
-    repo = Repo.clone_from(REPO_URL, to_path=LOCAL_STRAGE_PATH, branch = BRANCH)
+    Repo.clone_from(REPO_URL, to_path=LOCAL_STRAGE_PATH, branch=BRANCH)
     print("complete!\n")
 
 
 # リポジトリの読み込みと、テキストの分割
-
-ANALIZE_TARGET_PATH = os.getenv("ANALIZE_TARGET_PATH")
-
 loader = GenericLoader.from_filesystem(
     LOCAL_STRAGE_PATH + ANALIZE_TARGET_PATH,
     glob="**/*",
     suffixes=[".py"],
-    parser=LanguageParser(language=Language.PYTHON,parser_threshold=500),
-    )
+    parser=LanguageParser(language=Language.PYTHON, parser_threshold=500),
+)
 
 documents = loader.load()
 print(f"分割されたドキュメントの総数は:{len(documents)} 個\n")
 
 
 # 全てのファイルを読み込んで、テキストを分割する
-python_splitter = RecursiveCharacterTextSplitter.from_language(language=Language.PYTHON,chunk_size=2000,chunk_overlap=200)
+python_splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON, chunk_size=2000, chunk_overlap=200
+)
 documents = python_splitter.split_documents(documents)
 
 # インデックス作成
@@ -54,24 +69,23 @@ create_index_or_pass()
 # ベクトル化とベクターDBに保存
 upsert_vector_from_documents(documents)
 
-
-# retriever定義　TODO:インデックス初期化処理が重複しているのでクラスにまとめたい
-index = pinecone.Index(INDEX_NAME)
-vectorstore = Pinecone(index, embeddings.embed_query,'testname')
-# 現状from_llmなので、これつけるとベクトル検索のためのプロンプトが、不安定になるのでプロンプトテンプレートを使用して対策する。search_type="mmr",serch_kwargs={"k":1}も最適にする
-retriever = vectorstore.as_retriever(search_type="mmr",serch_kwargs={"k":1})
+# retriever定義　
+vectorstore = Pinecone(index, embeddings.embed_query, "testname")
+retriever = vectorstore.as_retriever(search_type="mmr", serch_kwargs={"k": 1})
 
 # pinecone retrieverで会話するための初期化
-llm = ChatOpenAI(model_name="gpt-3.5-turbo")
-memory = ConversationSummaryMemory(llm=llm, memory_key="chat_history",return_messages=True)
-qa = ConversationalRetrievalChain.from_llm(llm,retriever=retriever,memory=memory)
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+memory = ConversationSummaryMemory(
+    llm=llm, memory_key="chat_history", return_messages=True
+)
 
+# 現状from_llmなので、これつけるとベクトル検索のためのプロンプトが、不安定になるのでプロンプトテンプレートを使用して対策する。search_type="mmr",serch_kwargs={"k":1}も最適にする
+qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
 
 # memory設定と問い合わせ
-question =input("このリポジトリに対する質問を入力してください。")
+question = input("このリポジトリに対する質問を入力してください。\n")
 result = qa(question)
 print(result["answer"])
-
 
 # ファイルが存在するか確認してから削除
 if os.path.exists(LOCAL_STRAGE_PATH):
